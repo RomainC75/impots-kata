c1f1f6167f6a492513248a219083a268ba6165de
Romain CHENARD <romainchenard@Romains-MacBook-Pro.local>
2025-09-23 15:48:17 +0200

    feat: 🎸 subroutes

--- internal/core/router/routing/helloRoutes.go
+++ internal/core/router/routing/helloRoutes.go
@@ -1,12 +1,32 @@
 package routing
 
 import (
+	"fmt"
 	routes "impots/internal/core/router/controllers"
 	"net/http"
 )
 
-func HelloRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("GET /hello", routes.HelloBasicRoute)
-	mux.HandleFunc("GET /helloquery/", routes.HelloRouteQuery)
-	mux.HandleFunc("POST /helloparam/{id}", routes.HelloRouteParam)
+func HelloRoutes() http.Handler {
+	helloMux := http.NewServeMux()
+	helloMux.HandleFunc("GET /", routes.HelloBasicRoute)
+	helloMux.HandleFunc("GET /query/", routes.HelloRouteQuery)
+	helloMux.HandleFunc("POST /param/{id}", routes.HelloRouteParam)
+
+	helloMux.Handle("POST /param-mid/{id}", (&Mid{}).Add(routes.HelloBasicRoute))
+
+	return helloMux
+}
+
+type Mid struct {
+	fn func(w http.ResponseWriter, r *http.Request)
+}
+
+func (m *Mid) Add(fn func(w http.ResponseWriter, r *http.Request)) *Mid {
+	m.fn = fn
+	return m
+}
+
+func (m *Mid) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("--> Go")
+	m.fn(w, r)
 }
