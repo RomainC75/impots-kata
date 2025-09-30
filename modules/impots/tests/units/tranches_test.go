d3c68ae6c3c31302febf5fb23439967ce6253fdd
Romain CHENARD <romainchenard@Romains-MacBook-Pro.local>
2025-09-30 07:35:22 +0200

    test: 💍 test cases

--- modules/impots/tests/units/tranches_test.go
+++ modules/impots/tests/units/tranches_test.go
@@ -15,9 +15,13 @@ type TranchesTC struct {
 
 var tcs []TranchesTC = []TranchesTC{
 	{10_000, 0},
+	{15_000, 500},
 	{20_000, 1000},
 	{30_000, 2800},
+	{35_000, 4050},
 	{40_000, 5300},
+	{50_000, 7800},
+	{60_000, 10_800},
 }
 
 func TestTranches(t *testing.T) {
